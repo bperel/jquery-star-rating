c3fcccab00c61955f838b4be2bf5ecd320cfde9f
Bruno Perel <brunoperel@gmail.com>
2014-07-10 11:54:34 +0200

    cleanup

--- src/rating.js
+++ src/rating.js
@@ -16,12 +16,12 @@
 
 ;(function($){
     $.fn.rating = function(callback){
-        
+
         callback = callback || function(){};
 
         // each for all item
         this.each(function(i, v){
-            
+
             $(v).data('rating', {callback:callback})
                 .bind('init.rating', $.fn.rating.init)
                 .bind('set.rating', $.fn.rating.set)
@@ -29,77 +29,75 @@
                 .trigger('init.rating');
         });
     };
-    
+
     $.extend($.fn.rating, {
-        init: function(e){
+        init: function(){
             var el = $(this),
                 list = '',
                 isChecked = null,
-                childs = el.children(),
+                children = el.children(),
                 i = 0,
-                l = childs.length;
-            
+                l = children.length;
+
             for (; i < l; i++) {
-                list = list + '<a class="star" title="' + $(childs[i]).val() + '" />';
-                if ($(childs[i]).is(':checked')) {
-                    isChecked = $(childs[i]).val();
-                };
-            };
-            
-            childs.hide();
-            
+                list = list + '<a class="star" title="' + $(children[i]).val() + '" />';
+                if ($(children[i]).is(':checked')) {
+                    isChecked = $(children[i]).val();
+                }
+            }
+
+            children.hide();
+
             el
                 .append('<div class="stars">' + list + '</div>')
                 .trigger('set.rating', isChecked);
-            
-            $('a', el).bind('click', $.fn.rating.click);            
+
+            $('a', el).bind('click', $.fn.rating.click);
             el.trigger('hover.rating');
         },
         set: function(e, val) {
             var el = $(this),
                 item = $('a', el),
                 input = undefined;
-            
+
             if (val) {
                 item.removeClass('fullStar');
-                
-                input = item.filter(function(i){
+
+                input = item.filter(function(){
                     if ($(this).attr('title') == val)
                         return $(this);
                     else
                         return false;
                 });
-                
+
                 input
                     .addClass('fullStar')
                     .prevAll()
                     .addClass('fullStar');
             }
-            
-            return;
         },
-        hover: function(e){
+        hover: function(){
             var el = $(this),
                 stars = $('a', el);
-            
-            stars.bind('mouseenter', function(e){
+
+            stars.bind('mouseenter', function(){
                 // add tmp class when mouse enter
                 $(this)
                     .addClass('tmp_fs')
                     .prevAll()
                     .addClass('tmp_fs');
-                
+
                 $(this).nextAll()
                     .addClass('tmp_es');
             });
-            
-            stars.bind('mouseleave', function(e){
+
+            stars.bind('mouseleave', function(){
                 // remove all tmp class when mouse leave
                 $(this)
                     .removeClass('tmp_fs')
                     .prevAll()
                     .removeClass('tmp_fs');
-                
+
                 $(this).nextAll()
                     .removeClass('tmp_es');
             });
@@ -110,22 +108,19 @@
                 container = el.parent().parent(),
                 inputs = container.children('input'),
                 rate = el.attr('title');
-                
-            matchInput = inputs.filter(function(i){
-                if ($(this).val() == rate)
-                    return true;
-                else
-                    return false;
+
+            var matchInput = inputs.filter(function(){
+                return $(this).val() == rate;
             });
-            
+
             matchInput
                 .attr('checked', true)
-				.siblings('input').attr('checked', false);
-            
+                .siblings('input').attr('checked', false);
+
             container
                 .trigger('set.rating', matchInput.val())
                 .data('rating').callback(rate, e);
         }
     });
-    
+
 })(jQuery);
